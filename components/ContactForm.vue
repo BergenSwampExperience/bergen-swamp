8a29ae3072d70a3b92c165ee9dd4329f26bf2d9e
YahongZheng <36079586+YahongZheng@users.noreply.github.com>
2020-10-25 16:22:29 -0400

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="mailto:yxz8501@rit.edu" method="get" enctype="text/plain">
+  <form method="post" action="contact-form-handler.php">
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
